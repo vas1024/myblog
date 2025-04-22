214bf12c233f566a45c516366bfb45b7ca8be0c9
i5nuc <ip14922@gmail.com>
2025-04-22 16:47:34 +0300

    init commit

---
+++ src/main/java/myblog/Main.java
@@ -0,0 +1,9 @@
+package myblog;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+  public static void main(String[] args) {
+    ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+  }
+}
